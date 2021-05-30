fa13eb1091b9eac1857c82c83e6bbfc617ac8fb8
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-05-30 21:52:26 +0300

    Use console to let the user input any statement they wants to be encrypted

---
+++ App.java
@@ -0,0 +1,13 @@
+//import console to let the user input
+import java.io.Console;
+
+public class App {
+    public static void main(String[] args) {
+        Console console = System.console();
+
+        System.out.println("Please Enter the message to Encrypt");
+        String textEncrypt = System.console();//lets the user input
+
+
+    }
+}
