c755af2635fc9f7fd1deafb1c8519cf218970dfc
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-05-30 22:42:55 +0300

    Add method to checkif the data entered is string

--- App.java
+++ App.java
@@ -1,4 +1,5 @@
 //import console to let the user input
+//Encrypting class takes input and
 import java.io.Console;
 
 public class App {
@@ -7,9 +8,20 @@ public class App {
 
         System.out.println("Please Enter the message to Encrypt");
         String text = console.readLine();//lets the user input
+//        text[] allText = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
+        public boolean checkType (text.checkType){//public method since it is available to everyone
+            if (text !=string){
+                System.out.println("Please Enter a string ");
+
+            }
+            else{
+                System.out.println("You have entered correct datatype:");
+            }
+
+        }
 
         String encryptedText;
-        String decryptedText;
+
         //declarig an array to hold the encrypting key
         System.out.println("Kindly enter the encrypting key you would love to use:");
         String encryptingKey = console.readLine() ;
---
+++ Cipher.java
@@ -0,0 +1,8 @@
+//decoding class takes encryptedText and decrypts it.
+public class Cipher {
+    String decryptedText;
+
+    public String getDecryptedText() {
+        return decryptedText;
+    }
+}
